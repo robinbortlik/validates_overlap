2384d54a96f4365e349a2aaf792cae64a5d36960
Robin Bortlik <robinbortlik@gmail.com>
2012-04-27 16:14:00 +0100

    travis test fix

--- spec/dummy/spec/models/meeting_spec.rb
+++ spec/dummy/spec/models/meeting_spec.rb
@@ -14,18 +14,6 @@ describe Meeting do
   end
 
   context "Validation" do
-
-    OVERLAP_TIME_RANGES = {
-        "has same starts_at and ends_at" => ['2011-01-05'.to_date, '2011-01-08'.to_date],
-        "starts before starts_at and ends after ends_at" => ['2011-01-04'.to_date, '2011-01-09'.to_date],
-        "starts before starts_at and ends inside" => ['2011-01-04'.to_date, '2011-01-06'.to_date],
-        "starts inside and ends after ends_at" => ['2011-01-06'.to_date, '2011-01-09'.to_date],
-        "starts inside and ends inside" => ['2011-01-06'.to_date, '2011-01-07'.to_date],
-        "starts at same time and ends inside" => ['2011-01-05'.to_date, '2011-01-07'.to_date],
-        "starts inside and ends at same time" => ['2011-01-06'.to_date, '2011-01-08'.to_date],
-        "starts before and ends at time of start" => ['2011-01-03'.to_date, '2011-01-05'.to_date],
-        "starts at time of end and ends after" => ['2011-01-08'.to_date, '2011-01-19'.to_date]
-    }
     
     OVERLAP_TIME_RANGES.each do |description, time_range|
       it "is not valid if exists meeting which #{description}" do
